9a027320baf451405301892e78a5f0fa5e5acd06
jjangse <jjangse@192.168.1.10>
2020-08-24 17:52:52 +0900

    Chapter7

---
+++ Chapter7_inheritance/src/inheritance/VIPCustomer.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+public class VIPCustomer extends Customer {
+
+	double salesRatio;
+	private int agentID;
+	
+	public VIPCustomer(String customerName, int customerID) {
+		super(customerName, customerID);
+		
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+	}
+	
+	@Override
+	public int calPrice(int price) {
+		bonusPoint += price*bonusRatio;
+		return (int)(price*(1-salesRatio));
+	}
+
+	public int getAgentID() {
+		return agentID;
+	}
+
+	public void setAgentID(int agentID) {
+		this.agentID = agentID;
+	}
+	
+	
+}
